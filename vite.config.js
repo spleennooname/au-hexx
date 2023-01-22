9eb1ee972cd22b8e8249cf83cfaf621edc434657
Andrea Bovo <spleen666@gmail.com>
2023-01-22 17:22:03 +0100

    bump deps, fix doctype

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,15 @@
 // vite.config.js
 
 import { defineConfig } from 'vite'
-
 import glsl from 'vite-plugin-glsl'
-import obfuscator from 'rollup-plugin-obfuscator';
 
 export default defineConfig(({ command, mode }) => {
   return {
-    server: {
-      https: true
-    },
     plugins: [
-      glsl()
-    ],
-    build: {
-      // cssCodeSplit: true,
-      minify: 'esbuild',
-      chunkSizeWarningLimit: 1000,
-      rollupOptions: {
-        input: {
-          index: './index.html'
-        },
-        output: {
-          plugins: [
-            obfuscator({
-              globalOptions: {
-                // Your javascript-obfuscator options here
-                // Will be applied on the whole bundle. Set to `false` to disable
-                // See what's allowed: https://github.com/javascript-obfuscator/javascript-obfuscator
-              }
-            })
-          ]
-        }
-      }
-    }
+      glsl({
+        warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
+        compress: false // the resulting shader code
+      })
+    ]
   }
 })
